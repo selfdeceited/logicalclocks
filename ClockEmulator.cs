76b40f33805ddbd28421c1e2726fc13830be4ebd
aryzhikov <selfdeceited@gmail.com>
2021-12-05 18:17:48 +0300

    small refactoring

--- ClockEmulator.cs
+++ ClockEmulator.cs
@@ -2,6 +2,7 @@ public class ClockEmulator
 {
     private static readonly Random Random = new Random();
     
+     // todo: think how to avoid defaultClockValue. Do several event types?
     public void StartSequence<T>(List<ISubject<Event<T>>> subjects, T defaultClockValue)
     {
         // subscribe to each other
@@ -10,29 +11,31 @@ public class ClockEmulator
                 if (subject != other)
                     subject.Subscribe(other);
 
-        
+
         // subscribe to its own data flow
         subjects.ForEach(subject => GetFlow<T>(defaultClockValue).Subscribe(subject));
     }
     
-    private static IObservable<Event<T>> GetFlow<T>(T defaultClockValue, TimeSpan? clockJitter = null)
+    private static IObservable<Event<T>> GetFlow<T>(T defaultClockValue, FlowOptions? flowOptions = null)
     {
-        // let's go with persistent shift by now
-        var jitter = clockJitter ?? GetDefaultJitter();
+        var options = flowOptions ?? new FlowOptions(5, TimeSpan.FromMilliseconds(2), GetDefaultJitter());
 
         var observables = Observable
-            .Range(0, 5) // todo: parametrize
-            .DelaySubscription(TimeSpan.FromMilliseconds(2)) // todo: parametrize
+            .Range(0, options.NumberOfEvents)
+            .DelaySubscription(options.SubscriptionDelay)
             .Timestamp()
             .Select(i => new Event<T>(
                 new object().GetHashCode(),
                 defaultClockValue,
-                new[] { "original" }, // geez I want discriminated unions to happen
+                new[] { "original" }, // todo: fix code smell
                 i.Timestamp.DateTime))
-            .Select(e => e.WithTimestamp(e.Timestamp.Add(jitter)));
+            // let's go with persistent shift by now
+            .Select(e => e.WithTimestamp(e.Timestamp.Add(options.ClockJitter)));
 
         return observables;
     }
 
     private static TimeSpan GetDefaultJitter() => TimeSpan.FromMilliseconds(Random.Next(0, 4));
 }
+
+ public record FlowOptions(int NumberOfEvents, TimeSpan SubscriptionDelay, TimeSpan ClockJitter);
--- VectorClockSubject.cs
+++ VectorClockSubject.cs
@@ -1,4 +1,5 @@
-public record Vector(Dictionary<string, int> _) {
+public record Vector(Dictionary<string, int> _) // todo: consider moving to ValueTuple[]?
+{
     public Vector DeepClone() {
         var innerClone = _
             .Select(x => (x.Key, x.Value))
@@ -7,10 +8,10 @@ public record Vector(Dictionary<string, int> _) {
         return new Vector(innerClone);
     }
 
-    public override string ToString()
-    {
-        return string.Join(',', _.Select(c => $"\n\t{c.Key} {c.Value}")) + "\n";
-    }
+    public override string ToString() => string.Join(',', _.Select(c => $"\n\t{c.Key} {c.Value}")) + "\n";
+    public void Set(string key, int value) =>  _[key] = value;
+    public (string name, int value)[] GetAll() => _.Select(x => (x.Key, x.Value)).ToArray();
+    public int Get(string name) => _[name];
 }
 
 public class VectorClockSubject : ClockSubject<Vector>
@@ -23,15 +24,14 @@ public class VectorClockSubject : ClockSubject<Vector>
 
     protected override void OnNextInternal(Event<Vector> e)
     {
-        var myVector = LocalClock._.Single(x => x.Key == this.Name);
+        var myVector = LocalClock.Get(this.Name);
         if (e.Origins.Last() == "original") {   
-            LocalClock._[myVector.Key] = myVector.Value + 1;
+            LocalClock.Set(Name, myVector + 1);
         } else {
-            foreach (var remoteVector in e.LocalClock._){
-                var localVector = LocalClock._.Single(x => x.Key == remoteVector.Key);
-                LocalClock._[remoteVector.Key] = Math.Max(localVector.Value, remoteVector.Value);
+            foreach (var (remoteName, remoteValue) in e.LocalClock.GetAll()) {
+                LocalClock.Set(remoteName, Math.Max(LocalClock.Get(remoteName), remoteValue));
             }
-            LocalClock._[myVector.Key] = myVector.Value + 1;
+            LocalClock.Set(Name, myVector + 1);
         }
     }
 }
